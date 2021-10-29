b1ff22ae0bd3f6be6096bccde975859f8c0ce221
Rachel Shim <cs899@cornell.edu>
2021-10-29 17:06:08 -0400

    make it look nicer

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 #!/usr/bin/python3
 
-import requests
-import gtfs_realtime_pb2
-import nyct_subway_pb2
-from google.protobuf import text_format
-import time
-import datetime
+import datetime, requests, time
+import gtfs_realtime_pb2, nyct_subway_pb2
 
 URL = "https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-l"
 key = "" # add MTA API key here
+
+def get_arrival_time_string(arrival):
+	now = time.time()
+	train_in = round((arrival - now)/60)
+	if train_in < -5:
+		return "--"
+	elif train_in < 1:
+		return "Now"
+	else:
+		return str(train_in) + "min"
+
+def parse_timestamp(timestamp):
+	return datetime.datetime.fromtimestamp(timestamp).strftime("%Y-%m-%d %H:%M:%S")
 
 def main():
 	while True:
@@ -19,7 +28,7 @@ def main():
 		message = gtfs_realtime_pb2.FeedMessage()
 		message.ParseFromString(resp.content)
 
-		closest_train = {
+		next_train = {
 			"brooklyn": 0,
 			"manhattan": 0
 		}
@@ -28,43 +37,25 @@ def main():
 			trip = entity.trip_update
 			if trip:
 				for time_update in trip.stop_time_update:
-					arrival_time = time_update.arrival.time
-					if time_update.stop_id == "L08S":	# brooklyn-bound bedford
-						closest_train["brooklyn"] = arrival_time if closest_train["brooklyn"] == 0 else min(arrival_time, closest_train["brooklyn"])
-					elif time_update.stop_id == "L08N":	# manhattan-bound beford
-						closest_train["manhattan"] = arrival_time if closest_train["manhattan"] == 0 else min(arrival_time, closest_train["manhattan"])
+					arrival = time_update.arrival.time
+					if time_update.stop_id == "L08S":	# Brooklyn-bound from Bedford Ave
+						next_train["brooklyn"] = arrival if next_train["brooklyn"] == 0 else min(arrival, next_train["brooklyn"])
+					elif time_update.stop_id == "L08N":	# Manhattan-bound from Bedford Ave
+						next_train["manhattan"] = arrival if next_train["manhattan"] == 0 else min(arrival, next_train["manhattan"])
 			else:
-				pass #TODO
+				pass # ignore stopped trains
 
-		next_brooklyn_train = datetime.datetime.fromtimestamp(closest_train["brooklyn"]).strftime("%Y-%m-%d %H:%M:%S")
-		next_manhattan_train = datetime.datetime.fromtimestamp(closest_train["manhattan"]).strftime("%Y-%m-%d %H:%M:%S")
-
-		now = time.time()
-		brooklyn_time = round((closest_train["brooklyn"] - now)/60)
-		manhattan_time = round((closest_train["manhattan"] - now)/60)
+		brooklyn_in = get_arrival_time_string(next_train["brooklyn"])
+		manhattan_in = get_arrival_time_string(next_train["manhattan"])
 		
-		if closest_train["brooklyn"] == 0 or brooklyn_time < -5:
-			b_min = "--"
-		elif brooklyn_time < 1:
-			b_min = "Now"
-		else:
-			b_min = str(brooklyn_time) + "min"
-
-		if closest_train["manhattan"] == 0 or manhattan_time < -5:
-			m_min = "--"
-		elif manhattan_time < 1:
-			m_min = "Now"
-		else:
-			m_min = str(manhattan_time) + "min"
-		
-		print("checked at: ", datetime.datetime.fromtimestamp(timestamp).strftime("%Y-%m-%d %H:%M:%S"))
-		print("the next brooklyn-bound train is due at: ", next_brooklyn_train)
-		print("the next manhattan-bound train is due at: ", next_manhattan_train)
-		print("next brooklyn train in: ", b_min)
-		print("next manhattan train in: ", m_min)
+		print(parse_timestamp(timestamp))
+		print("the next brooklyn-bound train is due at: ", parse_timestamp(next_train["brooklyn"]))
+		print("the next manhattan-bound train is due at: ", parse_timestamp(next_train["manhattan"]))
+		print("next brooklyn train in: ", brooklyn_in)
+		print("next manhattan train in: ", manhattan_in)
 		print("\n")
 
-		time.sleep(30)
+		time.sleep(15)
 
 
 if __name__ == "__main__":
