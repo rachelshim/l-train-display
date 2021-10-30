5943da95a67f7c88418b5d33ad78193eea5cd146
Rachel Shim <cs899@cornell.edu>
2021-10-29 21:24:12 -0400

    fix main

--- main.py
+++ main.py
@@ -4,26 +4,6 @@ import time, sys
 from rgbmatrix import graphics, RGBMatrix, RGBMatrixOptions
 from TrainUpdater import get_next_trains
 
-options = RGBMatrixOptions()
-options.rows = 32
-options.cols = 64
-options.chain_length = 1
-options.parallel = 1
-options.hardware_mapping = "adafruit-hat"
-
-matrix = RGBMatrix(options=options)
-
-dir_font = graphics.Font()
-dir_font.LoadFont("5x8.bdf")
-time_font = graphics.Font()
-time_font.LoadFont("5x8.bdf")
-
-l_train_color = graphics.Color(167, 169, 172)
-ont_color = graphics.Color(250, 200, 50)
-
-
-canvas = matrix.CreateFrameCanvas()
-
 # drawing 10x10 L train logo
 def draw_l_train_logo(x, y):
 	for j in range(3):
@@ -42,8 +22,27 @@ def draw_l_train_logo(x, y):
 
 def run():
 	print("Press CTRL-C to stop.")
-	while True:
 
+	options = RGBMatrixOptions()
+	options.rows = 32
+	options.cols = 64
+	options.chain_length = 1
+	options.parallel = 1
+	options.hardware_mapping = "adafruit-hat"
+
+	matrix = RGBMatrix(options=options)
+
+	dir_font = graphics.Font()
+	dir_font.LoadFont("5x8.bdf")
+	time_font = graphics.Font()
+	time_font.LoadFont("5x8.bdf")
+
+	l_train_color = graphics.Color(167, 169, 172)
+	ont_color = graphics.Color(250, 200, 50)
+
+	canvas = matrix.CreateFrameCanvas()
+
+	while True:
 		manhattan_in, brooklyn_in = get_next_trains()
 
 		draw_l_train_logo(2, 3)
@@ -63,3 +62,4 @@ if __name__ == "__main__":
 	except KeyboardInterrupt:
 		print("Exiting l-train-display\n")
 		sys.exit(0)
+		
