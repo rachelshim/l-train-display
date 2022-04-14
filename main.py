010c880b5458f4fe3a684e3b00357bf64ed44812
Rachel Shim <cs899@cornell.edu>
2022-04-13 22:48:08 -0400

    convert to spaces

--- main.py
+++ main.py
@@ -13,72 +13,72 @@ logging.basicConfig(filename="main.log", level=logging.DEBUG)
 
 # drawing 10x10 L train logo
 def draw_l_train_logo(x, y, canvas):
-	for j in range(3):
-		offset = j + 1
-		for i in range(offset, 10 - offset):
-			canvas.SetPixel(x + i, y + 2 - j, 117, 119, 122)
-			canvas.SetPixel(x + i, y + 8 + j, 117, 119, 122)
-	for j in range(3, 8):
-		for i in range(10):
-			if i == 4:
-				canvas.SetPixel(x + i, y + j, 255, 255, 255)
-			elif j == 7 and (i == 5 or i == 6):
-				canvas.SetPixel(x + i, y + j, 255, 255, 255)
-			else:
-				canvas.SetPixel(x + i, y + j, 117, 119, 122)
+    for j in range(3):
+        offset = j + 1
+        for i in range(offset, 10 - offset):
+            canvas.SetPixel(x + i, y + 2 - j, 117, 119, 122)
+            canvas.SetPixel(x + i, y + 8 + j, 117, 119, 122)
+    for j in range(3, 8):
+        for i in range(10):
+            if i == 4:
+                canvas.SetPixel(x + i, y + j, 255, 255, 255)
+            elif j == 7 and (i == 5 or i == 6):
+                canvas.SetPixel(x + i, y + j, 255, 255, 255)
+            else:
+                canvas.SetPixel(x + i, y + j, 117, 119, 122)
 
 def run():
-	logging.info("Starting l-train-display.")
+    logging.info("Starting l-train-display.")
 
-	train_updater = TrainUpdater(URL, mta_api_key.key)
+    train_updater = TrainUpdater(URL, mta_api_key.key)
 
-	options = RGBMatrixOptions()
-	options.rows = 32
-	options.cols = 64
-	options.chain_length = 1
-	options.parallel = 1
-	options.hardware_mapping = "adafruit-hat"
+    options = RGBMatrixOptions()
+    options.rows = 32
+    options.cols = 64
+    options.chain_length = 1
+    options.parallel = 1
+    options.hardware_mapping = "adafruit-hat"
 
-	matrix = RGBMatrix(options=options)
+    matrix = RGBMatrix(options=options)
 
-	dir_font = graphics.Font()
-	dir_font.LoadFont("5x8.bdf")
-	time_font = graphics.Font()
-	time_font.LoadFont("5x8.bdf")
+    dir_font = graphics.Font()
+    dir_font.LoadFont("5x8.bdf")
+    time_font = graphics.Font()
+    time_font.LoadFont("5x8.bdf")
 
-	l_train_color = graphics.Color(167, 169, 172)
-	font_color = graphics.Color(250, 200, 50)
+    l_train_color = graphics.Color(167, 169, 172)
+    font_color = graphics.Color(250, 200, 50)
 
-	canvas = matrix.CreateFrameCanvas()
+    canvas = matrix.CreateFrameCanvas()
 
-	while True:
-		manh, bkln = train_updater.get_next_trains()
-		canvas.Clear()
+    while True:
+        manh, bkln = train_updater.get_next_trains()
+        canvas.Clear()
 
-		manh_terminus = manh.terminus if manh else "--"
-		bkln_terminus = bkln.terminus if bkln else "--"
-		manh_next_train = manh.next_train if manh else 0
-		bkln_next_train = bkln.next_train if bkln else 0
+        manh_terminus = manh.terminus if manh else "--"
+        bkln_terminus = bkln.terminus if bkln else "--"
+        manh_next_train = manh.next_train if manh else 0
+        bkln_next_train = bkln.next_train if bkln else 0
 
-		draw_l_train_logo(2, 3, canvas)
-		draw_l_train_logo(2, 18, canvas)
-		graphics.DrawText(canvas, dir_font, 14, 12, font_color, manh_terminus)
-		graphics.DrawText(canvas, dir_font, 14, 27, font_color, bkln_terminus)
+        draw_l_train_logo(2, 3, canvas)
+        draw_l_train_logo(2, 18, canvas)
+        graphics.DrawText(canvas, dir_font, 14, 12, font_color, manh_terminus)
+        graphics.DrawText(canvas, dir_font, 14, 27, font_color, bkln_terminus)
 
-		manhattan_offset = 37 if len(manh_next_train) == 5 else 42
+        manhattan_offset = 37 if len(manh_next_train) == 5 else 42
                 brooklyn_offset = 37 if len(bkln_next_train) == 5 else 42
 
-		graphics.DrawText(canvas, time_font, manhattan_offset, 12, font_color, manh_next_train)
-		graphics.DrawText(canvas, time_font, brooklyn_offset, 27, font_color, bkln_next_train)
-		canvas = matrix.SwapOnVSync(canvas)
+        graphics.DrawText(canvas, time_font, manhattan_offset, 12, font_color, manh_next_train)
+        graphics.DrawText(canvas, time_font, brooklyn_offset, 27, font_color, bkln_next_train)
+        canvas = matrix.SwapOnVSync(canvas)
 
-		time.sleep(30)
+        time.sleep(30)
 
 
 if __name__ == "__main__":
-	try:
-		run()
-	except KeyboardInterrupt:
-		logging.info("Exiting l-train-display\n")
-		sys.exit(0)
-		
+    try:
+        run()
+    except KeyboardInterrupt:
+        logging.info("Exiting l-train-display\n")
+        sys.exit(0)
+        
--- train_updater.py
+++ train_updater.py
@@ -5,78 +5,78 @@ import logging
 import constants
 
 class TrainUpdater:
-	def __init__(self, url, key):
-		self.URL = url
-		self.key = key
+    def __init__(self, url, key):
+        self.URL = url
+        self.key = key
 
-	class Trip(object):
-		def __init__(self, terminus, direction, next_train):
-			self.terminus = terminus
-			self.direction = direction
-			self.next_train = next_train
+    class Trip(object):
+        def __init__(self, terminus, direction, next_train):
+            self.terminus = terminus
+            self.direction = direction
+            self.next_train = next_train
 
-	# get_next_trains pings the MTA API for the most up-to-date train arrivals at Bedford Ave
-	def get_next_trains(self):
-		try:
-			resp = requests.get(self.URL, headers={"x-api-key": self.key})
-			timestamp = time.Time()
+    # get_next_trains pings the MTA API for the most up-to-date train arrivals at Bedford Ave
+    def get_next_trains(self):
+        try:
+            resp = requests.get(self.URL, headers={"x-api-key": self.key})
+            timestamp = time.Time()
 
-			message = gtfs_realtime_pb2.FeedMessage()
-			message.ParseFromString(resp.content)
+            message = gtfs_realtime_pb2.FeedMessage()
+            message.ParseFromString(resp.content)
 
-			trips_to_bedford = []
+            trips_to_bedford = []
 
-			# parse through all the trips listed in this message
-			entities = message.entity
-			for entity in entities:
-				trip_update = entity.trip_update
-				if trip_update:
-					parsed_trip = parse_trip_update(trip_update)
-					if parsed_trip:
-						trips_to_bedford.append(parsed_trip)
+            # parse through all the trips listed in this message
+            entities = message.entity
+            for entity in entities:
+                trip_update = entity.trip_update
+                if trip_update:
+                    parsed_trip = parse_trip_update(trip_update)
+                    if parsed_trip:
+                        trips_to_bedford.append(parsed_trip)
 
-			return parse_trips_to_bedford(trips_to_bedford)
-		except Exception as e:
-			logging.error(e)
-			return None, None
-
-
-	# parse_trip_update parses a trip_update entity in the GTFS feed response.
-	# if the trip specified in the trip_update stops at Bedford Av, it returns
-	# a Trip object. If the trip_update does not stop at Bedford Av, it returns None.
-	def parse_trip_update(trip_update):
-		direction = trip_update.trip.nyct_trip_descriptor.direction
-		final_stop_id = ""
-		max_stop_sequence = 0
-		next_train_at_bedford = 0
-
-		for stop_time_update in trip_update:
-			if stop_time_update.stop_sequence > max_stop_sequence:
-				max_stop_sequence = stop_time_update.stop_sequence
-				final_stop_id = stop_time_update.stop_id
-			if stop_time_update.stop_id == constants.BEDFORD_AV_NORTH or stop_time_update.stop_id == constants.BEDFORD_AV_SOUTH:
-				next_train_at_bedford = stop_time_update.arrival.time
-
-		if next_train_at_bedford == 0:
-			return None
-		else:
-			return Trip(final_stop_id, direction, next_train_at_bedford)
+            return parse_trips_to_bedford(trips_to_bedford)
+        except Exception as e:
+            logging.error(e)
+            return None, None
 
 
-	# parse_trips_to_bedford returns the soonest trips in each direction (North and South)
-	# that stops at Bedford Av.
-	# if there are no trips to be found for one of the directions, return None for that direction.
-	def parse_trips_to_bedford(trips_to_bedford):
-		northbound_trips = filter(lambda x: x.direction == constants.DIRECTION_NORTH, trips_to_bedford)
-		southbound_trips = filter(lambda x: x.direction == constants.DIRECTION_SOUTH, trips_to_bedford)
+    # parse_trip_update parses a trip_update entity in the GTFS feed response.
+    # if the trip specified in the trip_update stops at Bedford Av, it returns
+    # a Trip object. If the trip_update does not stop at Bedford Av, it returns None.
+    def parse_trip_update(trip_update):
+        direction = trip_update.trip.nyct_trip_descriptor.direction
+        final_stop_id = ""
+        max_stop_sequence = 0
+        next_train_at_bedford = 0
 
-		sorted_northbound_trips = sorted(northbound_trips, key=lambda x: x.next_train)
-		sorted_southbound_trips = sorted(southbound_trips, key=lambda x: x.next_train)
+        for stop_time_update in trip_update:
+            if stop_time_update.stop_sequence > max_stop_sequence:
+                max_stop_sequence = stop_time_update.stop_sequence
+                final_stop_id = stop_time_update.stop_id
+            if stop_time_update.stop_id == constants.BEDFORD_AV_NORTH or stop_time_update.stop_id == constants.BEDFORD_AV_SOUTH:
+                next_train_at_bedford = stop_time_update.arrival.time
 
-		next_northbound_train = sorted_northbound_trips[0] if len(sorted_northbound_trips) > 0 else None
-		next_southbound_train = sorted_southbound_trips[0] if len(sorted_southbound_trips) > 0 else None
+        if next_train_at_bedford == 0:
+            return None
+        else:
+            return Trip(final_stop_id, direction, next_train_at_bedford)
 
-		return (next_northbound_train, next_southbound_train)
+
+    # parse_trips_to_bedford returns the soonest trips in each direction (North and South)
+    # that stops at Bedford Av.
+    # if there are no trips to be found for one of the directions, return None for that direction.
+    def parse_trips_to_bedford(trips_to_bedford):
+        northbound_trips = filter(lambda x: x.direction == constants.DIRECTION_NORTH, trips_to_bedford)
+        southbound_trips = filter(lambda x: x.direction == constants.DIRECTION_SOUTH, trips_to_bedford)
+
+        sorted_northbound_trips = sorted(northbound_trips, key=lambda x: x.next_train)
+        sorted_southbound_trips = sorted(southbound_trips, key=lambda x: x.next_train)
+
+        next_northbound_train = sorted_northbound_trips[0] if len(sorted_northbound_trips) > 0 else None
+        next_southbound_train = sorted_southbound_trips[0] if len(sorted_southbound_trips) > 0 else None
+
+        return (next_northbound_train, next_southbound_train)
 
 
 
