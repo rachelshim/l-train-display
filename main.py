c46e0ddd9e615e15c961654361b3040b6cbb048f
Rachel Shim <cs899@cornell.edu>
2022-04-16 21:58:44 -0400

    fix indents

--- main.py
+++ main.py
@@ -10,65 +10,65 @@ logging.basicConfig(filename="main.log", level=logging.DEBUG)
 
 # drawing 10x10 L train logo
 def draw_l_train_logo(x, y, canvas):
-	for j in range(3):
-		offset = j + 1
-		for i in range(offset, 10 - offset):
-			canvas.SetPixel(x + i, y + 2 - j, 117, 119, 122)
-			canvas.SetPixel(x + i, y + 8 + j, 117, 119, 122)
-	for j in range(3, 8):
-		for i in range(10):
-			if i == 4:
-				canvas.SetPixel(x + i, y + j, 255, 255, 255)
-			elif j == 7 and (i == 5 or i == 6):
-				canvas.SetPixel(x + i, y + j, 255, 255, 255)
-			else:
-				canvas.SetPixel(x + i, y + j, 117, 119, 122)
+    for j in range(3):
+        offset = j + 1
+        for i in range(offset, 10 - offset):
+            canvas.SetPixel(x + i, y + 2 - j, 117, 119, 122)
+            canvas.SetPixel(x + i, y + 8 + j, 117, 119, 122)
+    for j in range(3, 8):
+        for i in range(10):
+            if i == 4:
+                canvas.SetPixel(x + i, y + j, 255, 255, 255)
+            elif j == 7 and (i == 5 or i == 6):
+                canvas.SetPixel(x + i, y + j, 255, 255, 255)
+            else:
+                canvas.SetPixel(x + i, y + j, 117, 119, 122)
 
 def run():
-	logging.info("Starting l-train-display.")
+    logging.info("Starting l-train-display.")
 
-	options = RGBMatrixOptions()
-	options.rows = 32
-	options.cols = 64
-	options.chain_length = 1
-	options.parallel = 1
-	options.hardware_mapping = "adafruit-hat"
+    options = RGBMatrixOptions()
+    options.rows = 32
+    options.cols = 64
+    options.chain_length = 1
+    options.parallel = 1
+    options.hardware_mapping = "adafruit-hat"
 
-	matrix = RGBMatrix(options=options)
+    matrix = RGBMatrix(options=options)
 
-	dir_font = graphics.Font()
-	dir_font.LoadFont("5x8.bdf")
-	time_font = graphics.Font()
-	time_font.LoadFont("5x8.bdf")
+    dir_font = graphics.Font()
+    dir_font.LoadFont("5x8.bdf")
+    time_font = graphics.Font()
+    time_font.LoadFont("5x8.bdf")
 
-	l_train_color = graphics.Color(167, 169, 172)
-	font_color = graphics.Color(250, 200, 50)
+    l_train_color = graphics.Color(167, 169, 172)
+    font_color = graphics.Color(250, 200, 50)
 
-	canvas = matrix.CreateFrameCanvas()
+    canvas = matrix.CreateFrameCanvas()
 
-	while True:
-		manhattan_in, brooklyn_in = get_next_trains()
-		canvas.Clear()
+    while True:
+        manhattan_in, brooklyn_in = get_next_trains()
+        canvas.Clear()
 
-		draw_l_train_logo(2, 3, canvas)
-		draw_l_train_logo(2, 18, canvas)
-		graphics.DrawText(canvas, dir_font, 14, 12, font_color, "MANH")
-		graphics.DrawText(canvas, dir_font, 14, 27, font_color, "BKLN")
+        draw_l_train_logo(2, 3, canvas)
+        draw_l_train_logo(2, 18, canvas)
+        graphics.DrawText(canvas, dir_font, 14, 12, font_color, "MANH")
+        graphics.DrawText(canvas, dir_font, 14, 27, font_color, "BKLN")
 
-		manhattan_offset = 37 if len(manhattan_in) == 5 else 42
-		brooklyn_offset = 37 if len(brooklyn_in) == 5 else 42
+        manhattan_offset = 37 if len(manhattan_in) == 5 else 42
+        brooklyn_offset = 37 if len(brooklyn_in) == 5 else 42
 
-		graphics.DrawText(canvas, time_font, manhattan_offset, 12, font_color, manhattan_in)
-		graphics.DrawText(canvas, time_font, brooklyn_offset, 27, font_color, brooklyn_in)
-		canvas = matrix.SwapOnVSync(canvas)
+        graphics.DrawText(canvas, time_font, manhattan_offset, 12, font_color, manhattan_in)
+        graphics.DrawText(canvas, time_font, brooklyn_offset, 27, font_color, brooklyn_in)
+        canvas = matrix.SwapOnVSync(canvas)
 
-		time.sleep(30)
+        time.sleep(30)
 
 
 if __name__ == "__main__":
-	try:
-		run()
-	except KeyboardInterrupt:
-		logging.info("Exiting l-train-display\n")
-		sys.exit(0)
-		
+    try:
+        run()
+    except KeyboardInterrupt:
+        logging.info("Exiting l-train-display\n")
+        sys.exit(0)
+        
