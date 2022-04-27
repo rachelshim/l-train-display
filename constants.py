6515c17e9e26c7e1224ee40a67ec4b9c4ce1113d
Rachel Shim <rshim@squarespace.com>
2022-04-27 08:32:20 -0700

    some fixes to main

--- constants.py
+++ constants.py
@@ -8,30 +8,30 @@ BEDFORD_AV_NORTH = "L08N"
 BEDFORD_AV_SOUTH = "L08S"
 
 L_STOPS = {
-	"L01": "8 Av",
-	"L02": "6 Av",
-	"L03": "14 St-Union Sq",
-	"L05": "3 Av",
-	"L06": "1 Av",
-	"L08": "Bedford Av",
-	"L10": "Lorimer St",
-	"L11": "Graham Av",
-	"L12": "Grand St",
-	"L13": "Montrose Av",
-	"L14": "Morgan Av",
-	"L15": "Jefferson St",
-	"L16": "DeKalb Av",
-	"L17": "Myrtle-Wyckoff Avs",
-	"L19": "Halsey St",
-	"L20": "Wilson Av",
-	"L21": "Bushwich Av-Aberdeen St",
-	"L22": "Broadway Junction",
-	"L24": "Atlantic Av",
-	"L25": "Sutter Av",
-	"L26": "Livonia Av",
-	"L27": "New Lots Av",
-	"L28": "East 105 St",
-	"L29": "Canarsie-Rockaway Pkwy"
+    "L01": "8 Av",
+    "L02": "6 Av",
+    "L03": "14 St-Union Sq",
+    "L05": "3 Av",
+    "L06": "1 Av",
+    "L08": "Bedford Av",
+    "L10": "Lorimer St",
+    "L11": "Graham Av",
+    "L12": "Grand St",
+    "L13": "Montrose Av",
+    "L14": "Morgan Av",
+    "L15": "Jefferson St",
+    "L16": "DeKalb Av",
+    "L17": "Myrtle-Wyckoff Avs",
+    "L19": "Halsey St",
+    "L20": "Wilson Av",
+    "L21": "Bushwich Av-Aberdeen St",
+    "L22": "Broadway Junction",
+    "L24": "Atlantic Av",
+    "L25": "Sutter Av",
+    "L26": "Livonia Av",
+    "L27": "New Lots Av",
+    "L28": "East 105 St",
+    "L29": "Canarsie-Rockaway Pkwy"
 }
 
 
@@ -41,7 +41,7 @@ FONT_WIDTH = 5
 FONT_HEIGHT = 8
 
 DISPLAY_SCROLL_SPEED = 0.05
-TEXT_HOLD_TIME_SECONDS = 2.5 
+TEXT_HOLD_TIME_SECONDS = 2.5
 
 # the space between the wrap-around for the text, in pixels
 TEXT_SPACER = 10
@@ -59,8 +59,8 @@ TEXT_SPACER = 10
 
 # every section that's not part of the scrolling text must be cleared
 VERTICAL_SPACERS = [
-	(0, 13),
-	(49, 63)
+    (0, 13),
+    (49, 63)
 ]
 
 L_LOGO_X = 2
--- main.py
+++ main.py
@@ -11,68 +11,67 @@ displayer = TrainDisplayer("5x8.bdf")
 
 lock = threading.Lock()
 next_trains_trip = {
-	"North": Trip("", Direction.NORTH, 0),
-	"South": Trip("", Direction.SOUTH, 0)
+    "North": None,
+    "South": None
 }
 
 def parse_arrival_time(arrival):
-	now = time.time()
-	arrival_in_min = round((arrival - now)/60)
-	if arrival_in_min < -5:
-		return "-"
-	else:
-		return str(arrival_in_min)
+    now = time.time()
+    arrival_in_min = round((arrival - now)/60)
+    if arrival_in_min < -5:
+        return "-"
+    else:
+        return str(arrival_in_min)
 
 def parse_trips_and_update_display():
-	while True:
-		lock.acquire()
-		trip_north = next_trains_trip["North"]
-		trip_south = next_trains_trip["South"]
-		lock.release()
+    while True:
+        lock.acquire()
+        trip_north = next_trains_trip["North"]
+        trip_south = next_trains_trip["South"]
+        lock.release()
 
-		north_stop = "No Trains"
-		south_stop = "No Trains"
-		north_time = "-"
-		south_time = "-"
-		if trip_north is not None:
-			north_stop = constants.L_STOPS[trip_north.terminus[:-1]]
-			north_time = parse_arrival_time(trip_north.next_train)
-		if trip_south is not None:
-			south_stop = constants.L_STOPS[trip_south.terminus[:-1]]
-			south_time = parse_arrival_time(trip_south.next_train)
+        north_stop = "No Trains"
+        south_stop = "No Trains"
+        north_time = "-"
+        south_time = "-"
+        if trip_north is not None:
+            north_stop = constants.L_STOPS[trip_north.terminus[:-1]]
+            north_time = parse_arrival_time(trip_north.next_train)
+        if trip_south is not None:
+            south_stop = constants.L_STOPS[trip_south.terminus[:-1]]
+            south_time = parse_arrival_time(trip_south.next_train)
 
-		displayer.update_display(north_stop, south_stop, north_time, south_time)
-		time.sleep(constants.DISPLAY_SCROLL_SPEED)
+        displayer.update_display(north_stop, south_stop, north_time, south_time)
+        time.sleep(constants.DISPLAY_SCROLL_SPEED)
 
 def update_next_trains():
-	while True:
-		train_north, train_south = updater.get_next_trains()
-		lock.acquire()
-		next_trains_trip["North"] = train_north
-		next_trains_trip["South"] = train_south
-		lock.release()
+    while True:
+        train_north, train_south = updater.get_next_trains()
+        lock.acquire()
+        next_trains_trip["North"] = train_north
+        next_trains_trip["South"] = train_south
+        lock.release()
 
-		time.sleep(constants.TRAIN_UPDATE_RATE_SECONDS)
+        time.sleep(constants.TRAIN_UPDATE_RATE_SECONDS)
 
 
 if __name__ == "__main__":
-	# TODO set up a decent logger
-	print("Starting l-train-display...")
+    # TODO set up a decent logger
+    print("Starting l-train-display...")
 
-	display_thread = threading.Thread(target=parse_trips_and_update_display)
-	update_thread = threading.Thread(target=update_next_trains)
-	display_thread.setDaemon(True)
-	update_thread.setDaemon(True)
+    try:
+        display_thread = threading.Thread(target=parse_trips_and_update_display)
+        update_thread = threading.Thread(target=update_next_trains)
+        display_thread.setDaemon(True)
+        update_thread.setDaemon(True)
 
-	display_thread.start()
-	update_thread.start()
+        display_thread.start()
+        update_thread.start()
+        while True:
+            pass
 
-	try:
-		while True:
-			pass
-		
-	except KeyboardInterrupt:
-		display_thread.stop()
-		update_thread.stop()
-		print("Exiting l-train-display...")
-		sys.exit(0)
+    except KeyboardInterrupt:
+        display_thread.stop()
+        update_thread.stop()
+        print("Exiting l-train-display...")
+        sys.exit(0)
